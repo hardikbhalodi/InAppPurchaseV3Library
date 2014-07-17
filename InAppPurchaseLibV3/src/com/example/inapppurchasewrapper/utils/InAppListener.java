fb0e095229c060eb06b9362f8c4ae56e2d36e4f6
hardik <hardik@hardik>
2014-07-17 15:21:26 +0530

    first commit

---
+++ InAppPurchaseLibV3/src/com/example/inapppurchasewrapper/utils/InAppListener.java
@@ -0,0 +1,16 @@
+package com.example.inapppurchasewrapper.utils;
+
+import java.util.List;
+
+public interface InAppListener {
+
+	void InAppInvetoryFinished(IabResult result, Inventory inventory);
+
+	void InAppPurchaseFinished(IabResult result, Purchase purchase);
+
+	void InAppConsumeFinished(Purchase purchase, IabResult result);
+
+	void InApponConsumeMultiFinished(List<Purchase> purchases,
+			List<IabResult> results);
+
+}
---
+++ InAppTestDemo/gen/com/example/inapptestdemo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.inapptestdemo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
